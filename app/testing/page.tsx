da78e50cccd5d9ce7cb4da3b42c5480f45c8f731
Horikaze <adrianyt659@gmail.com>
2024-03-16 23:07:49 +0100

    updated /texting

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,26 +1,18 @@
+// @ts-nocheck
 "use client";
-
 import { useState } from "react";
-import { achievementList, touhouDifficulty } from "../constants/games";
-import { scoreWR } from "../constants/wrScores";
 import { InputText } from "../mainComponents/InputText";
-
-interface RankValueRecord {
-  [key: string]: number;
-}
-
-interface CCValueRecord {
-  [key: string]: number;
-}
+import { gameCodeRecord, games618, touhouDifficulty } from "../constants/games";
+import { calculatePoints } from "../lib/calculatePoints";
+import { scoreWR } from "../constants/wrScores";
 
 export default function Search() {
   const [selectedGame, setSelectedGame] = useState(6);
-  const [selectedScore, setSelectedScore] = useState(0);
-  const [selectedRank, setSelectedRank] = useState("All");
-  const [selectedAchiv, setSelectedAchiv] = useState("All");
-  const [points, setPoints] = useState(0);
+  const [selectedScore, setSelectedScore] = useState("0");
+  const [selectedRank, setSelectedRank] = useState("Easy");
+  const [selectedAchiv, setSelectedAchiv] = useState("CC");
 
-  const [rankValueRecord, setRankValueRecord] = useState<RankValueRecord>({
+  const [rankValueRecord, setRankValueRecord] = useState({
     Easy: 1,
     Normal: 4,
     Hard: 4,
@@ -29,7 +21,7 @@ export default function Search() {
     Phantasm: 4,
   });
 
-  const [CCValueRecord, setCCValueRecord] = useState<CCValueRecord>({
+  const [CCValueRecord, setCCValueRecord] = useState({
     CC: 1,
     NM: 7,
     NB: 7,
@@ -37,171 +29,188 @@ export default function Search() {
     NNN: 28,
     NNNN: 36,
   });
-  const updateRankValue = (key: string, value: number) => {
-    setRankValueRecord((prevRankValueRecord) => ({
-      ...prevRankValueRecord,
-      [key]: value,
-    }));
-  };
 
-  const updateCCValue = (key: string, value: number) => {
-    setCCValueRecord((prevCCValueRecord) => ({
-      ...prevCCValueRecord,
-      [key]: value,
-    }));
-  };
-
-  const handleRankInputTextChange =
-    (key: string) => (event: React.ChangeEvent<HTMLInputTextElement>) => {
-      const value = parseInt(event.target.value, 10) || 0; // Parse InputText value as an integer
-      updateRankValue(key, value);
-    };
-
-  const handleCCInputTextChange =
-    (key: string) => (event: React.ChangeEvent<HTMLInputTextElement>) => {
-      const value = parseInt(event.target.value, 10) || 0; // Parse InputText value as an integer
-      updateCCValue(key, value);
-    };
-
-  const calculatePoints = () => {
+  const calculatePoints = (): number => {
     try {
-      const CCValue = CCValueRecord[selectedAchiv!];
-      const rankValue = rankValueRecord[selectedRank!];
+      //@ts-ignore
+      const rankValue = rankValueRecord[selectedRank];
       const scoreValue = (
-        (selectedScore / scoreWR[selectedGame][selectedRank]) *
+        (Number(selectedScore) / scoreWR[selectedGame][selectedRank]) *
         100
       ).toFixed(2);
-      const totalScore = Number(scoreValue) * rankValue * CCValue;
+      console.log(Number(selectedScore));
+      const totalScore =
+        //@ts-ignore
+        Number(scoreValue) * rankValue * CCValueRecord[selectedAchiv];
 
-      return totalScore.toFixed();
+      return Number(totalScore.toFixed());
     } catch (error) {
       console.log(error);
-      return "0";
+      return 0;
     }
   };
+  const points = calculatePoints() || 0;
 
   return (
-    <div className="flex flex-col gap-y-5">
-      <div>
-        <div>
-          <div className="flex flex-col items-center justify-center gap-3 md:flex-row">
-            <div className="grid w-full max-w-sm items-center gap-1.5">
-              <label htmlFor="game">Game</label>
-              <select
-                name="game"
-                onChange={(e) => {
-                  setSelectedGame(Number(e.target.value));
-                }}
-                value={selectedGame.toString()}
-                defaultValue="1"
-              >
-                {[6, 7, 8, 9, 10, 11, 12, 12.8, 13, 14, 15, 16, 17, 18].map(
-                  (game) => (
-                    <option key={game} value={game.toString()}>
-                      {game}
-                    </option>
-                  )
-                )}
-              </select>
-            </div>
-
-            <div className="grid w-full max-w-sm items-center gap-1.5">
-              <label htmlFor="score">Score</label>
-              <div className="gap-x-1 flex">
-                <InputText
-                  name="score"
-                  type="number"
-                  id="score"
-                  onChange={(e) => setSelectedScore(Number(e.target.value))}
-                  value={selectedScore.toString()}
-                />
-              </div>
-            </div>
-            <div className="grid w-full max-w-sm items-center gap-1.5">
-              <label htmlFor="rank">Rank</label>
-              <select
-                name="rank"
-                value={selectedRank}
-                onChange={(e) => setSelectedRank(e.target.value)}
-              >
-                {touhouDifficulty.map((diff) => (
-                  <option key={diff} value={diff}>
-                    {diff}
-                  </option>
-                ))}
-              </select>
-            </div>
-
-            <div className="grid w-full max-w-sm items-center gap-1.5">
-              <label htmlFor="achievement">Achievement</label>
-              <select
-                name="achievement"
-                value={selectedAchiv}
-                onChange={(e) => setSelectedAchiv(e.target.value)}
-              >
-                {achievementList.map((achiv) => (
-                  <option key={achiv} value={achiv}>
-                    {achiv}
-                  </option>
-                ))}
-              </select>
-            </div>
+    <div className="flex flex-col bg-primary w-full h-full p-3 rounded-md drop-shadow-md overflow-x-scroll gap-y-4">
+      <div className="flex gap-x-4">
+        <div className="grid w-full gap-1.5">
+          <p>Game</p>
+          <select
+            value={selectedGame}
+            onChange={(e) => {
+              setSelectedGame(Number(e.target.value));
+            }}
+            className="outline-white/20 focus:outline-white transition-all outline-none border-none bg-primary py-1 px-4 rounded-md cursor-pointer"
+          >
+            {Object.values(gameCodeRecord).map((game) => (
+              <option key={game + "select"} value={game}>
+                {game}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="grid w-full gap-1.5">
+          <p>Score</p>
+          <InputText
+            value={selectedScore}
+            onChange={(e) => {
+              if (isNaN(Number(e.target.value.trim()))) {
+                return;
+              }
+              setSelectedScore(e.target.value.trim());
+            }}
+            type="text"
+            placeholder="Score"
+            name="score"
+          />
+        </div>
+        <div className="grid w-full gap-1.5">
+          <p>Rank</p>
+          <select
+            value={selectedRank}
+            onChange={(e) => {
+              setSelectedRank(e.target.value);
+            }}
+            className="outline-white/20 focus:outline-white transition-all outline-none border-none bg-primary py-1 px-4 rounded-md cursor-pointer"
+          >
+            {touhouDifficulty.map((diff) => (
+              <option key={diff + "select"} value={diff}>
+                {diff}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="grid w-full gap-1.5">
+          <p>Achiv</p>
+          <select
+            value={selectedAchiv}
+            onChange={(e) => {
+              setSelectedAchiv(e.target.value);
+            }}
+            className="outline-white/20 focus:outline-white transition-all outline-none border-none bg-primary py-1 px-4 rounded-md cursor-pointer"
+          >
+            {Object.keys(CCValueRecord).map((achiv) => (
+              <option key={achiv + "select"} value={achiv}>
+                {achiv}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div className="flex flex-col items-end gap-y-3">
+        <div className="w-[500px] flex gap-x-3 text-sm justify-end">
+          <div className="flex flex-col justify-center text-base items-center">
+            <p>Score value =</p>
           </div>
-          <div className="w-full py-4 flex justify-between">
-            <div>
-              <p>Points: {calculatePoints()}</p>
-            </div>
-            <div className="flex flex-col">
-              <div className="flex relative text-xs">
-                <p className="absolute left-0">your score</p>
-                <p className="absolute left-28"> WR score</p>
-                <p className="absolute right-11">rank</p>
-                <p className="absolute right-0">achiv</p>-
-              </div>
-              <p>
-                ({selectedScore} / {scoreWR[selectedGame][selectedRank]} * 100)
-                * <span className="px-2">{rankValueRecord[selectedRank!]}</span>{" "}
-                * <span className="px-2"> {CCValueRecord[selectedAchiv!]}</span>
-              </p>
-            </div>
+          <div className="flex flex-col items-center">
+            <p>Score</p>
+            <p>{Number(selectedScore).toLocaleString()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <p className="text-2xl">/</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p>Wr score</p>
+            <p>{scoreWR[selectedGame][selectedRank].toLocaleString()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <p className="text-2xl">*</p>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <p>Fixed *</p>
+            <p>100</p>
+          </div>
+        </div>
+        <div className="w-[500px] flex gap-x-3 text-sm justify-end">
+          <div className="flex flex-col justify-center text-base items-center">
+            <p>Total points =</p>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <p>Score value</p>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <p className="text-2xl">*</p>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <p>Rank value</p>
+            <p>{rankValueRecord[selectedRank]}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <p className="text-2xl">*</p>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <p>CC value value</p>
+            <p>{CCValueRecord[selectedAchiv]}</p>
           </div>
         </div>
       </div>
-      <div>
-        <div>
-          <div className="flex flex-col md:flex-row justify-between gap-y-1.5">
-            <div>
-              <h2 className="text-center font-bold">Rank Value</h2>
-              <ul>
-                {Object.entries(rankValueRecord).map(([key, value]) => (
-                  <li key={key}>
-                    {key}:
-                    <InputText
-                      type="number"
-                      value={value}
-                      onChange={handleRankInputTextChange(key)}
-                    />
-                  </li>
-                ))}
-              </ul>
+      <div className="flex flex-row gap-x-4 justify-between">
+        <div className="flex flex-col gap-y-2">
+          <p className="text-center text-xl">Rank</p>
+          {Object.keys(rankValueRecord).map((r) => (
+            <div key={r + "change"} className="grid w-full gap-1.5">
+              <p>{r}</p>
+              <InputText
+                //@ts-ignore
+                value={rankValueRecord[r]}
+                onChange={(e) => {
+                  if (isNaN(Number(e.target.value.trim()))) {
+                    return;
+                  }
+                  setRankValueRecord((prevState) => ({
+                    ...prevState,
+                    [r]: e.target.value.trim(),
+                  }));
+                }}
+                type="text"
+                name="score"
+              />
             </div>
-
-            <div>
-              <h2 className="text-center font-bold">CC Value</h2>
-              <ul>
-                {Object.entries(CCValueRecord).map(([key, value]) => (
-                  <li key={key}>
-                    {key}:
-                    <InputText
-                      type="number"
-                      value={value}
-                      onChange={handleCCInputTextChange(key)}
-                    />
-                  </li>
-                ))}
-              </ul>
+          ))}
+        </div>
+        <div className="flex flex-col gap-y-2">
+          <p className="text-center text-xl">CC</p>
+          {Object.keys(CCValueRecord).map((r) => (
+            <div key={r + "change"} className="grid w-full gap-1.5">
+              <p>{r}</p>
+              <InputText
+                //@ts-ignore
+                value={CCValueRecord[r]}
+                onChange={(e) => {
+                  if (isNaN(Number(e.target.value.trim()))) {
+                    return;
+                  }
+                  setCCValueRecord((prevState) => ({
+                    ...prevState,
+                    [r]: e.target.value.trim(),
+                  }));
+                }}
+                type="text"
+                name="score"
+              />
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
