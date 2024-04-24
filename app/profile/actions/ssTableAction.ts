f62a1beb04ec99dc5402db42e611683b994df440
Horikaze <adrianyt659@gmail.com>
2024-04-24 19:10:31 +0200

    fixed getBrowser

--- app/profile/actions/ssTableAction.ts
+++ app/profile/actions/ssTableAction.ts
@@ -31,9 +31,20 @@ export const takeTableSS = async (url: string) => {
   }
 };
 
+const CHROMIUM_PATH =
+  "https://vomrghiulbmrfvmhlflk.supabase.co/storage/v1/object/public/chromium-pack/chromium-v123.0.0-pack.tar";
+
 async function getBrowser() {
   if (process.env.VERCEL_ENV === "production") {
-    const executablePath = await chromium.executablePath();
+    const chromium = await import("@sparticuz/chromium-min").then(
+      (mod) => mod.default
+    );
+
+    const puppeteerCore = await import("puppeteer-core").then(
+      (mod) => mod.default
+    );
+
+    const executablePath = await chromium.executablePath(CHROMIUM_PATH);
 
     const browser = await puppeteerCore.launch({
       args: chromium.args,
@@ -43,6 +54,8 @@ async function getBrowser() {
     });
     return browser;
   } else {
+    const puppeteer = await import("puppeteer").then((mod) => mod.default);
+
     const browser = await puppeteer.launch();
     return browser;
   }
